ef80828c1975b1d966acc939aac797c1056964d5
Cloudiiiii <Yusufhafeji33@gmail.com>
2021-10-06 11:55:41 -0400

    commit?
    yes commit.

--- IC3HJKYHP1/IC3HJKYHP1/Program.cs
+++ IC3HJKYHP1/IC3HJKYHP1/Program.cs
@@ -11,18 +11,50 @@
  *      
  */
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System;
 
-namespace IC3HJKYHP1
+namespace inClass3
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a number for a shape. Enter 1 for a square, 2 for a rectangle, and 3 for a triangle: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            static int Shapes(int a)
+            {
+                switch (a)
+                {
+                    case 1:
+                        Console.WriteLine("Please enter the length of the square side: ");
+                        int squareLength = Convert.ToInt32(Console.ReadLine());
+                        return squareLength;
+                    case 2:
+                        Console.WriteLine("Please enter the length of one of the rectangle sides: ");
+                        int rectangleLength1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Please eneter the length of the other rectangle side: ");
+                        int rectangleLength2 = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 3:
+                        Console.WriteLine("Please enter the length of one of the triangle sides: ");
+                        int triangleLength1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Please enter the length of one of the other triangle sides: ");
+                        int triangleLength2 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Please enter the length of the last triangle side: ");
+                        int triangleLength3 = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+
+            static int PerimeterShape(int a)
+            {
+
+            }
         }
     }
 }
