72642ee0e7a51a74afc8c94e9cf2e25b4242e873
Hyunjin Kim <69027218+jennn613@users.noreply.github.com>
2021-10-06 11:59:47 +0900

    fix switch statement

--- IC3HJKYHP1/IC3HJKYHP1/Program.cs
+++ IC3HJKYHP1/IC3HJKYHP1/Program.cs
@@ -23,38 +23,52 @@ namespace inClass3
     {
         static void Main(string[] args)
         {
+
+            int num;
+
             Console.WriteLine("Enter a number for a shape. Enter 1 for a square, 2 for a rectangle, and 3 for a triangle: ");
-            int num = Convert.ToInt32(Console.ReadLine());
+            num = Convert.ToInt32(Console.ReadLine());
 
-            static int Shapes(int a)
+            switch (num)
             {
-                switch (a)
-                {
-                    case 1:
-                        Console.WriteLine("Please enter the length of the square side: ");
-                        int squareLength = Convert.ToInt32(Console.ReadLine());
-                        return squareLength;
-                    case 2:
-                        Console.WriteLine("Please enter the length of one of the rectangle sides: ");
-                        int rectangleLength1 = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Please eneter the length of the other rectangle side: ");
-                        int rectangleLength2 = Convert.ToInt32(Console.ReadLine());
-                        break;
-                    case 3:
-                        Console.WriteLine("Please enter the length of one of the triangle sides: ");
-                        int triangleLength1 = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Please enter the length of one of the other triangle sides: ");
-                        int triangleLength2 = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Please enter the length of the last triangle side: ");
-                        int triangleLength3 = Convert.ToInt32(Console.ReadLine());
-                        break;
-                }
-            }
+                case 1:
+                    Console.WriteLine("Please enter the length of the square side: ");
+                    int squareLength = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("The perimeter of the selected shape is: " + PerimeterShape(squareLength));
+                    break;
 
-            static int PerimeterShape(int a)
-            {
+                case 2:
+                    Console.WriteLine("Please enter the length of one of the rectangle sides: ");
+                    int rectangleLength1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Please eneter the length of the other rectangle side: ");
+                    int rectangleLength2 = Convert.ToInt32(Console.ReadLine());
+                    break;
 
+                case 3:
+                    Console.WriteLine("Please enter the length of one of the triangle sides: ");
+                    int triangleLength1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Please enter the length of one of the other triangle sides: ");
+                    int triangleLength2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Please enter the length of the last triangle side: ");
+                    int triangleLength3 = Convert.ToInt32(Console.ReadLine());
+                    break;
             }
+            
+
+        }
+        static int PerimeterShape(int a)
+        {
+
+        }
+
+        static int PerimeterShape(int a, b)
+        {
+
+        }
+
+        static int PerimeterShape(int a, b, c)
+        {
+
         }
     }
 }
