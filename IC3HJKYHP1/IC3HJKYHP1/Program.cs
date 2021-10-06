bd3d12978f6b8e3fafe1281dedd3af17c2ab5e95
Hyunjin Kim <69027218+jennn613@users.noreply.github.com>
2021-10-06 11:20:28 +0900

    create files

---
+++ IC3HJKYHP1/IC3HJKYHP1/Program.cs
@@ -0,0 +1,28 @@
+﻿/* 
+ * Program ID: inClass3
+ * 
+ * Program Authors:
+ *      Yusuf Hafeji
+ *      Hyunjin Kim
+ * 
+ * 
+ * Revision History:
+ *      created Oct 6, 2021 by Yusuf Hafeji, Hyunjin Kim
+ *      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IC3HJKYHP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
